8c26563546b455ccf1699280d0b223d0049b96aa
Your Name <crrojasserrano@gmail.com>
2021-05-24 18:01:05 -0400

    initial commit

--- apps/app_favourite_books/models.py
+++ apps/app_favourite_books/models.py
@@ -4,7 +4,7 @@ from apps.app_login_register.models import User
 # Create your models here.
 class Book(models.Model):
     title = models.CharField(max_length=255)
-    description = models.TextField(default="Sin descripción")
+    description = models.TextField()
     uploaded_by = models.ForeignKey(User, related_name="books_uploaded", on_delete = models.CASCADE)
     users_who_like = models.ManyToManyField(User, related_name="liked_books")
     created_at = models.DateTimeField(auto_now_add=True)
